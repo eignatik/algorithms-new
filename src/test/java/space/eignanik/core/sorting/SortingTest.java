1ff33afe0364f580b94facb04630415e766f4d16
eignatik <evg.ignatik@gmail.com>
2017-09-05 13:36:36 +0300

    Added basic structure of merge sorting (doesn't work yet)

--- src/test/java/space/eignanik/core/sorting/SortingTest.java
+++ src/test/java/space/eignanik/core/sorting/SortingTest.java
@@ -2,6 +2,7 @@ package space.eignanik.core.sorting;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import space.eignatik.core.sorting.MergeSorting;
 import space.eignatik.core.sorting.Sorting;
 import space.eignatik.core.sorting.InsertionSorting;
 
@@ -13,14 +14,20 @@ public class SortingTest {
     @DataProvider(name = "arraysToSort")
     public Object[][] getArraysToSort() {
         return new Object[][] {
-                {new int[]{1, 5, 3, 9, 7}, new int[]{1, 3, 5, 7, 9}},
-                {new int[]{1, 5, 3, 9, 7, 12, 6, 8, 44, 1, 0}, new int[]{0, 1, 1, 3, 5, 6, 7, 8, 9, 12, 44}}
+                {new int[]{1, 5, 3, 9, 7, 4}, new int[]{1, 3, 4, 5, 7, 9}},
+                {new int[]{1, 5, 3, 9, 7, 12, 11, 6, 8, 44, 1, 0}, new int[]{0, 1, 1, 3, 5, 6, 7, 8, 9, 11, 12, 44}}
         };
     }
 
     @Test(dataProvider = "arraysToSort")
-    public void testIfInsertionSortingWorksCorrectly(int[] arrayToSort, int[] expectedArray) throws Exception {
+    public void insertionSortingWorksCorrectly(int[] arrayToSort, int[] expectedArray) throws Exception {
         int[] result = new InsertionSorting(arrayToSort).sort();
+        assertTrue(Arrays.equals(expectedArray, result));
+    }
+
+    @Test(dataProvider = "arraysToSort")
+    public void mergeSortingWorksCorrectly(int[] arrayToSort, int[] expectedArray) throws Exception {
+        int[] result = new MergeSorting(arrayToSort).sort();
         assertTrue(Arrays.equals(expectedArray, result));
     }
 }
